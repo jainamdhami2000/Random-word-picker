f7501a22b2e6b557fd0b27d8693c8c2ab293e1d0
Jainam Dhami <jainamdhami2000@gmail.com>
2020-11-12 14:49:33 +0530

    .

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Random-word-picker
 ## It is a command-line app to help in preparation for GRE for those who don't have flashcards
+### Run it by typing python words.py in the folder
--- words.py
+++ words.py
@@ -1,15 +1,14 @@
 import random
 print('If you dont know say n')
 
-words = ['Abate', 'Aberrant', 'Abeyance', 'Abscond', 'Abstemious', 'Admonish', 'Adulterate', 'Aesthetic', 'Aggregate', 'Alacrity', 'Alleviate', 'Amalgamate', 'Ambiguous', 'Ambivalence', 'Ameliorate', 'Anachronism', 'Analogous', 'Anarchy', 'Anomalous', 'Antipathy', 'Apathy', 'Appease', 'Apprise', 'Approbation', 'Appropriate', 'Arduous', 'Artless', 'Ascetic', 'Assiduous', 'Assuage', 'Attenuate', 'Audacious', 'Austere', 'Autonomous', 'Aver', 'Banal', 'Belie', 'Beneficent', 'Bolster', 'Bombastic', 'Boorish', 'Burgeon', 'Burnish', 'Buttress', 'Capricious', 'Castigation', 'Catalyst',
-         'Caustic', 'Chicanery', 'Coagulate', 'Coda', 'Cogent', 'Commensurate', 'Compendium', 'Complaisant', 'Compliant', 'Conciliatory', 'Condone', 'Confound', 'Connoisseur', 'Contention', 'Contentious', 'Contrite', 'Conundrum', 'Converge', 'Convoluted', 'Craven', 'Daunt', 'Decorum', 'Default', 'Deference', 'Delineate', 'Denigrate', 'Deride', 'Derivative', 'Desiccate', 'Desultory', 'Deterrent', 'Diatribe', 'Dichotomy', 'Diffidence', 'Diffuse', 'Digression', 'Dirge', 'Disabuse', 'Discerning', 'Discordant', 'Discredit', 'Discrepancy', 'Discrete', 'Disingenuous', 'Disinterested']
+words = ['Abate', 'Aberrant', 'Abeyance', 'Abscond', 'Abstemious', 'Admonish', 'Adulterate', 'Aesthetic', 'Aggregate', 'Alacrity', 'Alleviate', 'Amalgamate', 'Ambiguous', 'Ambivalence', 'Ameliorate', 'Anachronism', 'Analogous', 'Anarchy', 'Anomalous', 'Antipathy', 'Apathy', 'Appease', 'Apprise', 'Approbation', 'Appropriate', 'Arduous', 'Artless', 'Ascetic', 'Assiduous', 'Assuage', 'Attenuate', 'Audacious', 'Austere', 'Autonomous', 'Aver', 'Banal', 'Belie', 'Beneficent', 'Bolster', 'Bombastic', 'Boorish', 'Burgeon', 'Burnish', 'Buttress', 'Capricious', 'Castigation', 'Catalyst', 'Caustic', 'Chicanery', 'Coagulate',
+         'Coda', 'Cogent', 'Commensurate', 'Compendium', 'Complaisant', 'Compliant', 'Conciliatory', 'Condone', 'Confound', 'Connoisseur', 'Contention', 'Contentious', 'Contrite', 'Conundrum', 'Converge', 'Convoluted', 'Craven', 'Daunt', 'Decorum', 'Default', 'Deference', 'Delineate', 'Denigrate', 'Deride', 'Derivative', 'Desiccate', 'Desultory', 'Deterrent', 'Diatribe', 'Dichotomy', 'Diffidence', 'Diffuse', 'Digression', 'Dirge', 'Disabuse', 'Discerning', 'Discordant', 'Discredit', 'Discrepancy', 'Discrete', 'Disingenuous', 'Disinterested', 'Disjointed', 'Dismiss', 'Disparage', 'Disparate', 'Dissemble', 'Disseminate']
 outof = len(words)
-print(outof)
 score = 0
 while True:
     choice = random.choice(words)
     print(f'Say the meaning of:{choice}    {score}/{outof}')
-    response = input(f'Do you know it? ')
+    response = input(f'Do you know? ')
     if (response == 'n'):
         if score < outof / 3:
             remark = 'Poor'
@@ -25,5 +24,5 @@ while True:
         words.remove(choice)
         score += 1
         if(len(words) == 0):
-            print('You scored full!!!!!!!!')
+            print('You scored full!!!!!!!!🤩')
             break
